54f71ffda5c809c7fc5d94aee0e231b38118c9d5
eurqueta <eurqueta@gmail.com>
2016-04-04 23:59:54 -0300

    Integración con bootstrap

--- app/router.js
+++ app/router.js
@@ -8,6 +8,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route("orders", {path: "/orders"});
   this.route("poll");
+  this.route('about');
+  this.route('contact');
 });
 
 export default Router;
